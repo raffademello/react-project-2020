66ac3aebd0b464583b73811261c4bdb26171cd7a
webmaster <raffa.demello@gmail.com>
2020-10-15 10:39:53 -0300

    atualizacao

---
+++ project/src/components/Scores.jsx
@@ -0,0 +1,19 @@
+import React, { PureComponent } from 'react'
+
+export default class Scores extends PureComponent {
+    constructor(props) {
+        super(props);
+    }
+    state = {
+        isLoading: true,
+        scores: [],
+        balls:0
+    };
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
--- project/src/views/home/Home.jsx
+++ project/src/views/home/Home.jsx
@@ -1,13 +1,68 @@
 import React, { Component } from "react";
 import { Header, Segment, Container } from "semantic-ui-react";
+import axios from "axios";
+import footballBall from "../../assets/img/s-l400.jpg";
 
 class Home extends Component {
+  constructor() {
+    super();
+  }
+
+  state = {
+    isLoading: true,
+    scores: [],
+    balls:0
+  };
+  componentDidMount() {
+    const API_URL = "https://sportwatch-soccer.p.rapidapi.com/getResults/cl";
+    const headers = {
+      "x-rapidapi-host": "sportwatch-soccer.p.rapidapi.com",
+      "x-rapidapi-key": "548e1cd59amsh44e1e9822978385p1c5132jsn3f298bda43ad",
+      useQueryString: true,
+    };
+    axios
+      .get(API_URL, {
+        headers: headers,
+      })
+      .then((response) => {
+        this.setState({
+          scores: response.data["Results"],
+          isLoading: false,
+        });
+      });
+  }
   render() {
+    const results = this.state.scores;
+    //const URL_IMG = "/../../assets/img/s-l400.jpg"
+    console.log(results);
+    const fbStyle ={
+      backgroundImage:'url('+footballBall+') no-repeat center'
+    }
     return (
       <>
+      <h2>Champions League Scores</h2>
         <Segment textAlign="center" style={{ padding: "1em 0em" }} vertical>
-          <Header as="h1">Welcome</Header>
-          <Header sub>Lorem ipsum dolor sit amet, consectetur</Header>
+          <div className="results">
+            {results.map((result) => (
+              <div className="item">
+                  <div className="team home-team">
+                    {result.homeTeam}
+                  </div>
+                  <div className="score-wrap">
+                      <div class="score">
+                          {result.scoreHome}
+                      </div>
+                      <span>x</span>
+                    <div class="score">
+                          {result.scoreAway}
+                      </div>
+                  </div>
+                  <div className="team away-team">
+                    {result.awayTeam}
+                  </div>
+              </div>
+            ))}
+          </div>
         </Segment>
       </>
     );
